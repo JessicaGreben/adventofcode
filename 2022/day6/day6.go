e4b779b3b4cbb41626a5ef079430dd5780edc200
jessicagreben <jessicagrebens@gmail.com>
2022-12-06 05:54:55 -0800

    add solution for part 2 day 6 2022

--- 2022/day6/day6.go
+++ 2022/day6/day6.go
@@ -4,6 +4,10 @@ func part1() int {
 	return findPacketMarker(dataInput)
 }
 
+func part2() int {
+	return findMessage(dataInput)
+}
+
 func findPacketMarker(data string) int {
 	f := map[string]int{}
 	var i int
@@ -21,6 +25,30 @@ func findPacketMarker(data string) int {
 		}
 		f[string(data[i])]++
 		if len(f) == 4 {
+			return i + 1
+		}
+		i++
+	}
+	return -1
+}
+
+func findMessage(data string) int {
+	f := map[string]int{}
+	var i int
+	for i < 14 {
+		f[string(data[i])]++
+		i++
+	}
+	if len(f) == 14 {
+		return 0
+	}
+	for i < len(data) {
+		f[string(data[i-14])]--
+		if f[string(data[i-14])] == 0 {
+			delete(f, string(data[i-14]))
+		}
+		f[string(data[i])]++
+		if len(f) == 14 {
 			return i + 1
 		}
 		i++
--- 2022/day6/day6_test.go
+++ 2022/day6/day6_test.go
@@ -10,6 +10,12 @@ func TestPart1(t *testing.T) {
 	}
 }
 
+func TestPart2(t *testing.T) {
+	if want, got := 2193, part2(); want != got {
+		t.Errorf("want: %d, got: %d", want, got)
+	}
+}
+
 func TestPacketMarker(t *testing.T) {
 	testCases := []struct {
 		name   string
@@ -46,6 +52,47 @@ func TestPacketMarker(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			if want, got := tc.output, findPacketMarker(tc.input); want != got {
+				t.Errorf("want: %v, got: %v", want, got)
+			}
+		})
+	}
+}
+func TestMessage(t *testing.T) {
+	testCases := []struct {
+		name   string
+		input  string
+		output int
+	}{
+
+		{
+			"1",
+			"mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+			19,
+		},
+		{
+			"2",
+			"bvwbjplbgvbhsrlpgdmjqwftvncz",
+			23,
+		},
+		{
+			"3",
+			"nppdvjthqldpwncqszvftbrmjlhg",
+			23,
+		},
+		{
+			"4",
+			"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+			29,
+		},
+		{
+			"5",
+			"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
+			26,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if want, got := tc.output, findMessage(tc.input); want != got {
 				t.Errorf("want: %v, got: %v", want, got)
 			}
 		})
