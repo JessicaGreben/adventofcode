8b5e597b619f2d861abf77de8275558ebf1d8e31
jessicagreben <jessicagrebens@gmail.com>
2022-12-29 16:40:59 -0800

    add day 14 2022

---
+++ 2022/day14/day14_test.go
@@ -0,0 +1,29 @@
+package day14
+
+import "testing"
+
+func TestPart1(t *testing.T) {
+	if want, got := 592, part1(); want != got {
+		t.Errorf("want: %d, got: %d", want, got)
+	}
+}
+
+func TestPart1Example(t *testing.T) {
+	pathPoints, minY := convertLines(getLines("testinput.txt"))
+	if want, got := 24, countSand(pathPoints, minY); want != got {
+		t.Errorf("want: %d, got: %d", want, got)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	if want, got := 30367, part2(); want != got {
+		t.Errorf("want: %d, got: %d", want, got)
+	}
+}
+
+func TestPart2Example(t *testing.T) {
+	pathPoints, minY := convertLines(getLines("testinput.txt"))
+	if want, got := 93, countSandPart2(pathPoints, minY); want != got {
+		t.Errorf("want: %d, got: %d", want, got)
+	}
+}
