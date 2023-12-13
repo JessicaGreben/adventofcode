d0b55c530b9b93dab7b451588bf41c09b9f5645a
jessicagreben <jessicagrebens@gmail.com>
2023-12-13 08:21:01 -0800

    update template with int64

--- template/part1/solution_test.go
+++ template/part1/solution_test.go
@@ -6,7 +6,7 @@ func TestSolutionPart1(t *testing.T) {
 	testCases := []struct {
 		name  string
 		input string
-		want  int
+		want  int64
 	}{
 		{"1", "", -1},
 		{"2", "", -1},
@@ -17,6 +17,7 @@ func TestSolutionPart1(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
+			t.Skip()
 			out, err := processLine(tc.input)
 			if err != nil {
 				t.Fatal(err)
@@ -28,21 +29,23 @@ func TestSolutionPart1(t *testing.T) {
 	}
 
 	t.Run("input_test", func(t *testing.T) {
+		t.Skip()
 		out, err := solution("../input_test.txt")
 		if err != nil {
 			t.Fatal(err)
 		}
-		if want, got := 0, out; want != got {
+		if want, got := int64(0), out; want != got {
 			t.Errorf("want=%d, got=%d", want, got)
 		}
 	})
 
 	t.Run("input", func(t *testing.T) {
+		t.Skip()
 		out, err := solution("../input.txt")
 		if err != nil {
 			t.Fatal(err)
 		}
-		if want, got := 0, out; want != got {
+		if want, got := int64(0), out; want != got {
 			t.Errorf("want=%d, got=%d", want, got)
 		}
 	})
--- template/part2/solution.go
+++ template/part2/solution.go
@@ -4,10 +4,10 @@ import (
 	"github.com/jessicagreben/adventofcode/pkg/input"
 )
 
-func solution(file string) (int, error) {
-	return input.ForEachLine(file, processLine)
+func solution(file string) (int64, error) {
+	return input.ForEachLineInt64(file, processLine)
 }
 
-func processLine(line string) (int, error) {
+func processLine(line string) (int64, error) {
 	return -1, nil
 }
