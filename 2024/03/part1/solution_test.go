4049bf80684b1b7a6c7f1f3691584b0f7ddc40f7
jessicagreben <jessicagrebens@gmail.com>
2024-12-03 06:18:12 -0800

    Add solution for 2024 day 3

---
+++ 2024/03/part1/solution_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestSolutionPart1(t *testing.T) {
+	t.Run("input_test", func(t *testing.T) {
+		out, err := solution("example_input.txt")
+		if err != nil {
+			t.Fatal(err)
+		}
+		if want, got := int64(161), out; want != got {
+			t.Errorf("want=%d, got=%d", want, got)
+		}
+	})
+
+	t.Run("input", func(t *testing.T) {
+		out, err := solution("../input.txt")
+		if err != nil {
+			t.Fatal(err)
+		}
+		if want, got := int64(170068701), out; want != got {
+			t.Errorf("want=%d, got=%d", want, got)
+		}
+	})
+}
---
+++ 2024/03/part2/solution.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/jessicagreben/adventofcode/pkg/input"
+)
+
+func solution(file string) (int64, error) {
+	lines, err := input.GetLines(file)
+	if err != nil {
+		return -1, err
+	}
+
+	regex := `do\(\)|don't\(\)|mul\(\d+,\d+\)`
+	re := regexp.MustCompile(regex)
+	var sum int64
+	do := true
+	for _, line := range lines {
+		matches := re.FindAllString(line, -1)
+		for _, m := range matches {
+			if m == "do()" {
+				do = true
+				continue
+			}
+			if m == "don't()" {
+				do = false
+				continue
+			}
+			if do {
+				// each part should look like this mul(x,y)
+				parts := strings.FieldsFunc(m, splitOn)
+				if len(parts) != 3 {
+					return -1, fmt.Errorf("wrong len parts, want=3, got=%v", len(parts))
+				}
+				xInt, err := strconv.Atoi(parts[1])
+				if err != nil {
+					return -1, err
+				}
+				yInt, err := strconv.Atoi(parts[2])
+				if err != nil {
+					return -1, err
+				}
+				sum += int64(xInt) * int64(yInt)
+			}
+
+		}
+	}
+	return sum, nil
+}
+
+func splitOn(r rune) bool {
+	return r == '(' || r == ',' || r == ')'
+}
