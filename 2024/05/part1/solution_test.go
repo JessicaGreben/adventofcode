8a3a493588ef9bc97aedd9692c4eff9386e85413
jessicagreben <jessicagrebens@gmail.com>
2024-12-05 09:59:47 -0800

    Add solution 2024 day 5 part 1

---
+++ 2024/05/part1/solution_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestSolutionPart1(t *testing.T) {
+	t.Run("input_test", func(t *testing.T) {
+		out, err := solution("../example_input.txt")
+		if err != nil {
+			t.Fatal(err)
+		}
+		if want, got := int64(143), out; want != got {
+			t.Errorf("want=%v, got=%v", want, got)
+		}
+	})
+
+	t.Run("input", func(t *testing.T) {
+		out, err := solution("../input.txt")
+		if err != nil {
+			t.Fatal(err)
+		}
+		if want, got := int64(7198), out; want != got {
+			t.Errorf("want=%v, got=%v", want, got)
+		}
+	})
+}
