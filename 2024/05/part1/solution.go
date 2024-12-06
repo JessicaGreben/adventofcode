7aee3f9e8c650f307ab931eb142795764f1e7e1b
jessicagreben <jessicagrebens@gmail.com>
2024-12-06 10:10:49 -0800

    refactor 2024 day 5 part 1

--- 2024/05/part1/solution.go
+++ 2024/05/part1/solution.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"strconv"
+	"slices"
 	"strings"
 
 	fileinput "github.com/jessicagreben/adventofcode/pkg/input"
@@ -12,45 +12,79 @@ func solution(file string) (int64, error) {
 	if err != nil {
 		return -1, err
 	}
-	g, paths, err := parseLines(lines)
+	graph, paths, err := parseLines(lines)
 	if err != nil {
 		return -1, err
 	}
 
 	var result int64
 	for _, path := range paths {
-		var valid bool = true
-		for i := range path {
-			for j := i + 1; j < len(path); j++ {
-				a, b := path[i], path[j]
-				if _, ok := g[a+b]; !ok {
-					valid = false
-					continue
-				}
-			}
-		}
-		if valid {
-			pathMidStr := path[len(path)/2]
-			pathMidInt, err := strconv.Atoi(pathMidStr)
-			if err != nil {
-				return -1, nil
-			}
-			result += int64(pathMidInt)
+		if isValidPath(path, 0, graph) {
+			result += int64(path[len(path)/2])
 		}
 	}
 	return result, nil
 }
 
-func parseLines(lines []string) (adjList map[string]bool, paths [][]string, err error) {
-	adjList = map[string]bool{}
-	paths = [][]string{}
+func isValidPath(path []int64, pathIdx int, graph map[int64][]int64) bool {
+	q := []int64{path[0]}
+
+Outer:
+	for len(q) > 0 {
+		size := len(q)
+		for range size {
+			curr := q[0]
+			q = q[1:]
+
+			nextPathIdx := pathIdx + 1
+			if nextPathIdx >= len(path) {
+				return true
+			}
+
+			nextPath := path[nextPathIdx]
+			for _, edge := range graph[curr] {
+				if edge == nextPath {
+					pathIdx++
+					q = []int64{edge}
+					goto Outer
+				}
+
+				// make sure that each downstream item in the path doesn't need to come
+				// before the item  item we are adding to the queue.
+				correctOrder := true
+				for i := nextPathIdx; i < len(path); i++ {
+					downstreamPathValue := path[i]
+					nextsPathEdges := graph[downstreamPathValue]
+					if slices.Contains[[]int64](nextsPathEdges, edge) {
+						correctOrder = false
+					}
+				}
+				if correctOrder {
+					q = append(q, edge)
+				}
+			}
+		}
+	}
+	return false
+}
+
+func parseLines(lines []string) (adjList map[int64][]int64, paths [][]int64, err error) {
+	adjList = map[int64][]int64{}
+	paths = [][]int64{}
 	for _, line := range lines {
 		switch {
 		case strings.Contains(line, "|"):
-			parts := strings.Split(line, "|")
-			adjList[parts[0]+parts[1]] = true
+			parts, err := fileinput.ParseLineInt64(line, "|", 2)
+			if err != nil {
+				return adjList, paths, err
+			}
+			from, to := parts[0], parts[1]
+			adjList[from] = append(adjList[from], to)
 		case strings.Contains(line, ","):
-			parts := strings.Split(line, ",")
+			parts, err := fileinput.ParseLineInt64(line, ",", -1)
+			if err != nil {
+				return adjList, paths, err
+			}
 			paths = append(paths, parts)
 		}
 	}
